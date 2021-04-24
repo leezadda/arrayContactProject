1f471eb6af713cb6974bf2f8fe270c0bed1049d4
Leo Quezada <74736115+leezadda@users.noreply.github.com>
2021-04-23 21:59:26 -0500

    Add files via upload

--- last.py
+++ last.py
@@ -1,26 +1,27 @@
 """  Leo Quezada
      Program #4: The Phonebook program
+     COSC 1306
+     Spring 2021
 """
 
-def printMenu(): #DONE, print's menu
+def printMenu(): #DONE
     print("1. Print Contact List\n2. Add New Contact\n3. Delete Contact\n4. Modify Contact\n5. Exit")
 
-def readFromFile(filename): #DONE, reads file
+def readFromFile(filename): #DONE
     file = open(filename, "r")
     contacts = file.read()
     people = contacts.split("\n")
-    global newArray
     newArray = [] 
     for i in range(len(people)):
         newArray.append(people[i].split())
     return newArray
 
-def printContactList(contactList): #DONE, prints contact list
+def printContactList(contactList): #DONE
     for i in range(len(contactList)):
         print("Contact #", i+1)
         print("First name:", contactList[i][0] + ", Last name:", contactList[i][1] + ", Phone number:", contactList[i][2]+ ", Birthday:", contactList[i][3])
 
-def addContact(newArray): #DONE, this function receives as a parameter the list of contacts, adds contact, and does not return any value. 
+def addContact(newArray): #DONE
     first = input("Enter the First Name: ")
     last = input("Enter the Last Name: ")
     number = input("Enter the Phone Number: ")
@@ -28,22 +29,25 @@ def addContact(newArray): #DONE, this function receives as a parameter the list 
     newPerson = [first, last, number, birthday]
     newArray.append(newPerson)
 
-def deleteContact(newArray): #DONE, this function receives as a parameter the list of contacts, deletes contact, and does not return any value. 
+def deleteContact(newArray): #DONE
     contactIndex = int(input("Select the contact to be deleted: "))
-    if contactIndex < 1 or contactIndex > 4:
+    if contactIndex < 1 or contactIndex > len(newArray):
         print("Incorrect contact number")
     else:
-        newArray.remove(newArray[contactIndex-1])
+        newArray.pop(contactIndex-1)
         print("Contact", contactIndex, "deleted")
 
-def modifyContact(newArray): #DONE, this function receives as a parameter the list of contacts, modifies contact, and does not return any value. 
+def modifyContact(newArray): #this function receives as a parameter the list of contacts and does not return any value. 
     person = int(input("Select the contact to be changed: "))
-    first = input("Enter the First Name: ")
-    last = input("Enter the Last Name: ")
-    number = input("Enter the Phone Number: ")
-    birthday = input("Enter the Birthday: ")
-    newArray[person-1] = [first, last, number, birthday]
-    print("Contact", person, "was changed")
+    if person < 1 or person > len(newArray):
+        print("Incorrect contact number")
+    else:
+        first = input("Enter the First Name: ")
+        last = input("Enter the Last Name: ")
+        number = input("Enter the Phone Number: ")
+        birthday = input("Enter the Birthday: ")
+        newArray[person-1] = [first, last, number, birthday]
+        print("Contact", person, "was changed")
 
 ###################################
 filename = input("Enter the file name: ")
